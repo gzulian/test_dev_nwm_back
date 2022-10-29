0869d01611f160000a093a494a0f490240c5e430
gzulian <gzuliany@gmail.com>
2022-10-29 16:11:33 -0300

    ajuste service user

--- controller/index.js
+++ controller/index.js
@@ -4,13 +4,28 @@ const   express     = require('express'),
         router      = express.Router(),
         users       = require('../domain/services/service-user');
 
+
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+     if (token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.TOKEN_SECRET , (err, user) => {
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+    })
+}
 router.post('/login', users.Login);
 router.get('/authToken', users.AuthenticateToken);
-router.get('/users/:id', users.GetById);
+router.get('/users',authenticateToken, users.GetById);
 router.post('/users/', users.Store);
-router.patch('/users/transfers/:id', users.UpdateTransfersById);
-router.patch('/users/addressees/:id', users.UpdateAddresseesById);
-router.post('/users/transfer', users.Transfer);
+//router.patch('/users/transfers/:id', users.UpdateTransfersById);
+router.patch('/users/addressees',authenticateToken, users.UpdateAddresseesById);
+router.post('/users/transfer', authenticateToken,users.Transfer);
 
 
 module.exports = router;
--- domain/services/service-user.js
+++ domain/services/service-user.js
@@ -7,7 +7,6 @@ const repoUserPg = require('../../repositories/repository_aws_pgsql');
 
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-const { BaseException } = require('@angular-devkit/core');
 dotenv.config({ path: './.env' });
 
 exports.AuthenticateToken = async (req, res) =>{
@@ -36,26 +35,33 @@ exports.AuthenticateToken = async (req, res) =>{
 exports.Login = async (req, res) =>{
     let status = 'Success', errorCode ='', message='', data='', statusCode=0, resp={};
     try{
-        respOrm =await repoUserPg.pgsql_db.any('SELECT * FROM "User" ')
-        console.log(respOrm);
-
-
-        if(respOrm.err){
-            status = 'Failure', errorCode = respOrm.err.code, message = respOrm.err.messsage, statusCode = enum_.CODE_BAD_REQUEST;
+        respRepo =await repoUserPg.pgsql_db.one('SELECT id,password FROM "User" WHERE email = $1',[req.body.email])
+        if(!respRepo.err && respRepo.password == req.body.password){
+            //Generate JWT Token
+            const jwtToken =  jwt.sign({id:respRepo.id,email:req.body.email}, process.env.TOKEN_SECRET, { expiresIn: '1800m' });
+           
+            message = 'Success Response', data = {token:jwtToken}, statusCode = enum_.CODE_OK ;
+            
         }else{
-            message = 'Success Response', data = respOrm, statusCode = data.length > 0 ? enum_.CODE_OK : enum_.CODE_NO_CONTENT;
+            status = 'Failure', message = "", statusCode = enum_.CODE_NOT_FOUND;
         }
-        //resp = await magic.ResponseService(status,errorCode,message,data);
+
+        resp = {
+            status:status,
+            message:message,
+            data:data
+        }
+       
         return res.status(statusCode).send(resp);
     } catch(err) {
         console.log("err = ", err);
         //resp = await magic.ResponseService('Failure',enum_.CRASH_LOGIC,err,'');
-        return res.status(enum_.CODE_INTERNAL_SERVER_ERROR).send(resp);
+        return res.status(enum_.CODE_NOT_FOUND).send(resp);
     }
 }
 
 exports.Transfer = async (req, res,next) =>{
-    const user_id = 1;
+    const user_id = req.user.id;
     let status = 'Success', errorCode ='', message='', data='', statusCode=0, resp={};
     repoUserPg.pgsql_db.tx(async t => {
         respCurrentBalance = await  t.one(`SELECT current_balance   FROM "User" WHERE "id" = $1`,[1])
@@ -105,7 +111,7 @@ exports.Transfer = async (req, res,next) =>{
             message:message,
             data:data
         }
-        repoUserPg.pgsql_db.$pool.end();
+        //repoUserPg.pgsql_db.$pool.end();
         return res.status(statusCode).send(resp);
     })
     .catch(error => {
@@ -153,7 +159,7 @@ exports.Store = async (req, res) =>{
 exports.GetById = async (req, res) =>{
     let status = 'Success', errorCode ='', message='', data='', statusCode=0, resp={};
     try{
-        const id = req.params.id;
+        const id = req.user.id
         
         respOrm = await ormUser.GetById(id);
         if(respOrm && respOrm.err){
@@ -213,7 +219,7 @@ exports.UpdateTransfersById = async (req, res) =>{
 exports.UpdateAddresseesById = async (req, res) =>{
     let status = 'Success', errorCode ='', message='', data='', statusCode=0, resp={};
     try{
-        const id = req.params.id;
+        const id = req.user.id;
         if( id){
             respOrm = await ormUser.UpdateAddresseesById(id, req.body.addressees);
             if(respOrm.err){
